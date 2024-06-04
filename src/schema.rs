b239b848b3f8c18e564d58622199ca6a3c9b9508
AmrDeveloper <amr96@programmer.net>
2024-06-04 21:19:57 +0200

    feat: Remove lazy_static crate

--- src/schema.rs
+++ src/schema.rs
@@ -1,9 +1,10 @@
 use gitql_core::types::DataType;
-use lazy_static::lazy_static;
 use std::collections::HashMap;
+use std::sync::OnceLock;
 
-lazy_static! {
-    pub static ref TABLES_FIELDS_TYPES: HashMap<&'static str, DataType> = {
+pub fn tables_fields_types() -> &'static HashMap<&'static str, DataType> {
+    static HASHMAP: OnceLock<HashMap<&'static str, DataType>> = OnceLock::new();
+    HASHMAP.get_or_init(|| {
         let mut map = HashMap::new();
         map.insert("path", DataType::Text);
         map.insert("parent", DataType::Text);
@@ -12,16 +13,17 @@ lazy_static! {
         map.insert("is_file", DataType::Boolean);
         map.insert("size", DataType::Integer);
         map
-    };
+    })
 }
 
-lazy_static! {
-    pub static ref TABLES_FIELDS_NAMES: HashMap<&'static str, Vec<&'static str>> = {
+pub fn tables_fields_names() -> &'static HashMap<&'static str, Vec<&'static str>> {
+    static HASHMAP: OnceLock<HashMap<&'static str, Vec<&'static str>>> = OnceLock::new();
+    HASHMAP.get_or_init(|| {
         let mut map = HashMap::new();
         map.insert(
             "files",
             vec!["path", "parent", "extension", "is_dir", "is_file", "size"],
         );
         map
-    };
+    })
 }
