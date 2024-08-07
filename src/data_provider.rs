23c4d760648ebdd78ef256f6177edd0b194a4d0c
AmrDeveloper <amr96@programmer.net>
2024-08-07 20:42:15 +0200

    ref: Move select from file to another function

--- src/data_provider.rs
+++ src/data_provider.rs
@@ -17,81 +17,84 @@ impl FileDataProvider {
 
 impl DataProvider for FileDataProvider {
     fn provide(&self, table: &str, selected_columns: &[String]) -> Result<Vec<Row>, String> {
-        // If table is empty, thats mean it's a set of expressions query
-        if table.is_empty() {
-            return Ok(vec![Row { values: vec![] }]);
+        match table {
+            "files" => select_files(&self.paths, &self.excludes, selected_columns),
+            _ => Ok(vec![Row { values: vec![] }]),
         }
-
-        let mut files: Vec<String> = vec![];
-        for path in self.paths.iter() {
-            let files_tree = traverse_file_tree(path, &self.excludes);
-            for f in files_tree.iter() {
-                if files.contains(f) {
-                    continue;
-                }
-                files.push(f.to_string());
-            }
-        }
-
-        let names_len = selected_columns.len() as i64;
-        let mut rows: Vec<Row> = Vec::with_capacity(files.len());
-
-        for file in files {
-            let mut values: Vec<Value> = Vec::with_capacity(names_len as usize);
-            let path = Path::new(&file);
-
-            for index in 0..names_len {
-                let field_name = &selected_columns[index as usize];
-                if field_name == "path" {
-                    let file_path_string = path.to_str().unwrap_or("");
-                    values.push(Value::Text(file_path_string.to_string()));
-                    continue;
-                }
-
-                if field_name == "parent" {
-                    let parent_path = if let Some(parent) = path.parent() {
-                        parent.to_str().unwrap_or("")
-                    } else {
-                        ""
-                    };
-                    values.push(Value::Text(parent_path.to_string()));
-                    continue;
-                }
-
-                if field_name == "extension" {
-                    let extension = path.extension().and_then(|ext| ext.to_str()).unwrap_or("");
-                    values.push(Value::Text(extension.to_string()));
-                    continue;
-                }
-
-                if field_name == "is_dir" {
-                    values.push(Value::Boolean(path.is_dir()));
-                    continue;
-                }
-
-                if field_name == "is_file" {
-                    values.push(Value::Boolean(path.is_file()));
-                    continue;
-                }
-
-                if field_name == "size" {
-                    let file_size = if let Ok(meta_data) = std::fs::metadata(&file) {
-                        meta_data.len() as i64
-                    } else {
-                        0
-                    };
-                    values.push(Value::Integer(file_size));
-                    continue;
-                }
-
-                values.push(Value::Null);
-            }
-
-            rows.push(Row { values });
-        }
-
-        Ok(rows)
     }
+}
+
+fn select_files(
+    paths: &[String],
+    excludes: &[String],
+    selected_columns: &[String],
+) -> Result<Vec<Row>, String> {
+    let mut files: Vec<String> = vec![];
+    for path in paths {
+        let files_tree = traverse_file_tree(path, excludes);
+        for f in files_tree.iter() {
+            if files.contains(f) {
+                continue;
+            }
+            files.push(f.to_string());
+        }
+    }
+
+    let mut rows: Vec<Row> = Vec::with_capacity(paths.len());
+    for file in paths {
+        let mut values: Vec<Value> = Vec::with_capacity(selected_columns.len());
+        let path = Path::new(&file);
+
+        for column_name in selected_columns {
+            if column_name == "path" {
+                let file_path_string = path.to_str().unwrap_or("");
+                values.push(Value::Text(file_path_string.to_string()));
+                continue;
+            }
+
+            if column_name == "parent" {
+                let parent_path = if let Some(parent) = path.parent() {
+                    parent.to_str().unwrap_or("")
+                } else {
+                    ""
+                };
+                values.push(Value::Text(parent_path.to_string()));
+                continue;
+            }
+
+            if column_name == "extension" {
+                let extension = path.extension().and_then(|ext| ext.to_str()).unwrap_or("");
+                values.push(Value::Text(extension.to_string()));
+                continue;
+            }
+
+            if column_name == "is_dir" {
+                values.push(Value::Boolean(path.is_dir()));
+                continue;
+            }
+
+            if column_name == "is_file" {
+                values.push(Value::Boolean(path.is_file()));
+                continue;
+            }
+
+            if column_name == "size" {
+                let file_size = if let Ok(meta_data) = std::fs::metadata(file) {
+                    meta_data.len() as i64
+                } else {
+                    0
+                };
+                values.push(Value::Integer(file_size));
+                continue;
+            }
+
+            values.push(Value::Null);
+        }
+
+        rows.push(Row { values });
+    }
+
+    Ok(rows)
 }
 
 fn traverse_file_tree(dir_path: &str, excludes: &[String]) -> Vec<String> {
